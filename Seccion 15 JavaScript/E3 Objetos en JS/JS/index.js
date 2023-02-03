56adb83b6937436aaf45cc5de002415cdfa4aa77
luisfegz2 <luisfelipegaitanzambrano@gmail.com>
2023-02-03 15:45:47 -0500

    JavaScript Seccion 15

---
+++ Seccion 15 JavaScript/E3 Objetos en JS/JS/index.js
@@ -0,0 +1,16 @@
+// ! Tipos de Objetos en Javascript
+// * Un objeto es una agrupación de datos.
+// * Estos datos sentido entre sí.
+// * Los objetos no tienen variables sino propiedades.
+
+//?    Objeto Vacio
+const miPrimerObjeto = {}
+
+//todo Objeto 
+const miObjeto = {
+    unNumero: 39,
+    unString: 'Esta cadena de caracteres',
+    unaCondicion: true,
+}
+
+console.log(miObjeto);
